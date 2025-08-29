80bf718822b0fc54b8332773e1578e444660f7ff
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-08-29 09:56:18 -0300

    Update __init__.py

---
+++ views/__init__.py
@@ -0,0 +1,23 @@
+from . import (
+    administracao,
+    dashboard,
+    inspecao_extintores,
+    inspecao_mangueiras,
+    inspecao_scba,
+    inspecao_chuveiros,
+    inspecao_camaras_espuma,
+    historico,
+    utilitarios
+)
+
+__all__ = [
+    "administracao",
+    "dashboard",
+    "inspecao_extintores",
+    "inspecao_mangueiras",
+    "inspecao_scba",
+    "inspecao_chuveiros",
+    "inspecao_camaras_espuma",
+    "historico",
+    "utilitarios"
+]
