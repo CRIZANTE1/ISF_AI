e5500a2744e68b053c53dfa986bee18e84fec83b
CRIZANTE1 <CRISTIANFC2015@HOTMAIL.COM>
2025-11-15 15:15:11 -0300

    exclusoes

--- src/test/setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-// Limpa após cada teste
-afterEach(() => {
-  cleanup();
-});
-
